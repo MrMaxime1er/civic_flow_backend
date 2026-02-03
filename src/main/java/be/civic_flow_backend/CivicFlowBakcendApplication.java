1af1f23cabef898490fd5edc83988fa9d97209f1
victor morabito <victormorabito@gmail.com>
2026-02-03 21:42:28 +0100

    chore: initial commit

---
+++ src/main/java/be/civic_flow_backend/CivicFlowBakcendApplication.java
@@ -0,0 +1,13 @@
+package be.civic_flow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicFlowBakcendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivicFlowBakcendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/civic_flow_backend/CivicFlowBakcendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.civic_flow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicFlowBakcendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
